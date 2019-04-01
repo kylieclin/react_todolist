e05bfe8cc3c783626241949e593f94747c2c06dd
Kylie Lin <kylieclin@gmail.com>
2019-03-31 17:21:37 -0700

    todolist

--- src/components/app.js
+++ src/components/app.js
@@ -1,14 +1,61 @@
-import React from 'react';
+import 'materialize-css/dist/css/materialize.min.css';
+import 'materialize-css/dist/js/materialize.min';
+import React, {Component} from 'react';
 import '../assets/css/app.scss';
-import logo from '../assets/images/logo.svg';
+import Todolist from './todolist';
+import Input from './inputtodo';
 
-const App = () => (
-    <div>
-        <div className="app">
-            <img src={logo} className="logo rotate"/>
-            <h1>Welcome To React</h1>
+let id = 0;
+
+class App extends Component{
+    constructor(props){
+        super(props)
+        this.state={
+            todolist: []
+        }
+        this.addtodo = this.addtodo.bind(this);
+        this.deletetodo = this.deletetodo.bind(this);
+    }
+    addtodo(todolist){
+        todolist.id = id++;
+
+        this.setState({
+            todolist: [todolist.todo, ...this.state.todolist]
+        })
+        
+    }
+    deletetodo(todolist){
+        //delete by id
+        this.setState({
+            todolist: []
+        })
+    }
+    render(){
+
+        const classnames = {
+            todolist: "todolist",
+            todocontent: 'todocontent',
+            deletebtn: 'delete',
+            input: 'todoinput',
+            form: 'input-field',
+            col: 'col s12 m8'
+        }
+        return(
+        <div className="container">
+        <div className="app col s12 m8">
+            <div className="header">
+                <h1 className="center">Todo</h1>
+            </div>
+            <Input callback={this.addtodo} classes={classnames}/>
+            <div className="todos">
+               <Todolist todos={this.state.todolist} callback={this.deletetodo} classes={classnames}/> 
+            </div>
+            
         </div>
-    </div>
-);
+        </div>
+        )
+    }
+}
+ ;
 
 export default App;
